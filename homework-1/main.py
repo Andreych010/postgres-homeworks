d2eaabe9aff91c992c8656f10e22524801d9cbd0
Андрей Марчук <marchuk-8282@mail.ru>
2023-06-10 16:54:12 +0200

    Добавлен скрипт в main.py, который заполняет созданные таблицы данными из north_data

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,1 +1,37 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+
+import psycopg2, csv
+
+with psycopg2.connect(host="localhost", database="north", user="andrey", password="12345") as conn:
+    conn.autocommit = True
+    cur = conn.cursor()
+
+    with open('north_data/employees_data.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)  # Skip the header row.
+        for row in reader:
+            cur.execute(
+                "INSERT INTO employees VALUES (%s, %s, %s, %s, %s, %s)",
+                row
+            )
+
+    with open('north_data/orders_data.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            cur.execute(
+                "INSERT INTO orders VALUES (%s, %s, %s, %s, %s)",
+                row
+            )
+
+    with open('north_data/customers_data.csv', 'r') as f:
+        reader = csv.reader(f)
+        next(reader)
+        for row in reader:
+            cur.execute(
+                "INSERT INTO customers VALUES (%s, %s, %s)",
+                row
+            )
+
+conn.commit()
+conn.close()
